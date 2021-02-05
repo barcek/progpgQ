5ead0fe1c0050bcd3d7b5aa216b3e1a94a81c7f3
barcek <barcek@gmx.com>
2021-02-05 16:24:56 +0000

    feature: extended ./db/index.js w/ endPool

--- db/index.js
+++ db/index.js
@@ -5,9 +5,21 @@
 const path = require('path');
 
 const { Table } = require(path.resolve(__dirname, './table.js'));
+const { pool } = require(path.resolve(__dirname, './pool.js'));
+
+/*
+    Definitions
+*/
+
+const endPool = () => {
+    pool.end();
+};
 
 /*
     Exports
 */
 
-module.exports = { Table };
+module.exports = {
+    Table,
+    endPool
+};
